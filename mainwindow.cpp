0e06926ea9ddadfe961cc664791499c39f362e46
songhe jiao <1226383294@qq.com>
2021-06-24 14:15:36 +0800

    button clicked

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,10 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QMessageBox>
+#include <QFileDialog>
+#ifdef WIN32
+#pragma execution_character_set("utf-8")
+#endif
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -13,3 +18,16 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::on_pushButton_SelectModel_clicked()
+{
+    QString filepath = QString::null;
+    filepath = QFileDialog::getOpenFileName(this, "选择模型文件", ".", "Model(*om3);;Model(*stl);;All(*.*)");
+    QMessageBox::critical(NULL, QStringLiteral("Test"), QString(filepath), QMessageBox::Yes| QMessageBox::No, QMessageBox::Yes);
+}
+
+void MainWindow::on_pushButton_SelectPointCloud_clicked()
+{
+    QStringList filePathList;
+    filePathList = QFileDialog::getOpenFileNames(this, "选择点云文件", ".", "PointCloud(*ply)");
+}
