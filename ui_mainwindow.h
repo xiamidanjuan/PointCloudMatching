d45ef3066a65b661158fd336dfc5fbfa95564bdf
songhe jiao <1226383294@qq.com>
2022-07-18 10:13:14 +0800

    UI

--- ui_mainwindow.h
+++ ui_mainwindow.h
@@ -32,8 +32,13 @@ class Ui_MainWindow
 {
 public:
     QWidget *centralwidget;
+    QHBoxLayout *horizontalLayout_5;
+    QHBoxLayout *horizontalLayout_4;
+    QGridLayout *gridLayout;
+    QGraphicsView *graphicsView_DispWindow;
+    QVBoxLayout *verticalLayout_3;
     QGroupBox *groupBox;
-    QWidget *layoutWidget;
+    QGridLayout *gridLayout_4;
     QVBoxLayout *verticalLayout;
     QHBoxLayout *horizontalLayout;
     QLineEdit *lineEdit_PointCloudFilePath;
@@ -42,17 +47,15 @@ public:
     QLineEdit *lineEdit_ModelPath;
     QPushButton *pushButton_SelectModel;
     QGroupBox *groupBox_2;
+    QGridLayout *gridLayout_3;
     QTableView *tableView_DataList;
     QGroupBox *groupBox_3;
-    QWidget *layoutWidget1;
+    QGridLayout *gridLayout_5;
     QVBoxLayout *verticalLayout_2;
     QHBoxLayout *horizontalLayout_3;
     QPushButton *pushButton_CPMatch;
     QPushButton *pushButton_DataExport;
     QProgressBar *progressBar_MatchingProgress;
-    QWidget *gridLayoutWidget;
-    QGridLayout *gridLayout;
-    QGraphicsView *graphicsView_DispWindow;
     QMenuBar *menubar;
     QStatusBar *statusbar;
 
@@ -60,92 +63,16 @@ public:
     {
         if (MainWindow->objectName().isEmpty())
             MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
-        MainWindow->resize(1186, 779);
+        MainWindow->resize(1268, 831);
         centralwidget = new QWidget(MainWindow);
         centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
-        groupBox = new QGroupBox(centralwidget);
-        groupBox->setObjectName(QString::fromUtf8("groupBox"));
-        groupBox->setGeometry(QRect(760, 10, 411, 111));
-        layoutWidget = new QWidget(groupBox);
-        layoutWidget->setObjectName(QString::fromUtf8("layoutWidget"));
-        layoutWidget->setGeometry(QRect(10, 17, 391, 81));
-        verticalLayout = new QVBoxLayout(layoutWidget);
-        verticalLayout->setObjectName(QString::fromUtf8("verticalLayout"));
-        verticalLayout->setContentsMargins(0, 0, 0, 0);
-        horizontalLayout = new QHBoxLayout();
-        horizontalLayout->setObjectName(QString::fromUtf8("horizontalLayout"));
-        lineEdit_PointCloudFilePath = new QLineEdit(layoutWidget);
-        lineEdit_PointCloudFilePath->setObjectName(QString::fromUtf8("lineEdit_PointCloudFilePath"));
-
-        horizontalLayout->addWidget(lineEdit_PointCloudFilePath);
-
-        pushButton_SelectPointCloud = new QPushButton(layoutWidget);
-        pushButton_SelectPointCloud->setObjectName(QString::fromUtf8("pushButton_SelectPointCloud"));
-
-        horizontalLayout->addWidget(pushButton_SelectPointCloud);
-
-
-        verticalLayout->addLayout(horizontalLayout);
-
-        horizontalLayout_2 = new QHBoxLayout();
-        horizontalLayout_2->setObjectName(QString::fromUtf8("horizontalLayout_2"));
-        lineEdit_ModelPath = new QLineEdit(layoutWidget);
-        lineEdit_ModelPath->setObjectName(QString::fromUtf8("lineEdit_ModelPath"));
-
-        horizontalLayout_2->addWidget(lineEdit_ModelPath);
-
-        pushButton_SelectModel = new QPushButton(layoutWidget);
-        pushButton_SelectModel->setObjectName(QString::fromUtf8("pushButton_SelectModel"));
-
-        horizontalLayout_2->addWidget(pushButton_SelectModel);
-
-
-        verticalLayout->addLayout(horizontalLayout_2);
-
-        groupBox_2 = new QGroupBox(centralwidget);
-        groupBox_2->setObjectName(QString::fromUtf8("groupBox_2"));
-        groupBox_2->setGeometry(QRect(760, 126, 411, 481));
-        tableView_DataList = new QTableView(groupBox_2);
-        tableView_DataList->setObjectName(QString::fromUtf8("tableView_DataList"));
-        tableView_DataList->setGeometry(QRect(10, 20, 391, 441));
-        groupBox_3 = new QGroupBox(centralwidget);
-        groupBox_3->setObjectName(QString::fromUtf8("groupBox_3"));
-        groupBox_3->setGeometry(QRect(760, 610, 411, 111));
-        layoutWidget1 = new QWidget(groupBox_3);
-        layoutWidget1->setObjectName(QString::fromUtf8("layoutWidget1"));
-        layoutWidget1->setGeometry(QRect(10, 20, 391, 71));
-        verticalLayout_2 = new QVBoxLayout(layoutWidget1);
-        verticalLayout_2->setObjectName(QString::fromUtf8("verticalLayout_2"));
-        verticalLayout_2->setContentsMargins(0, 0, 0, 0);
-        horizontalLayout_3 = new QHBoxLayout();
-        horizontalLayout_3->setObjectName(QString::fromUtf8("horizontalLayout_3"));
-        pushButton_CPMatch = new QPushButton(layoutWidget1);
-        pushButton_CPMatch->setObjectName(QString::fromUtf8("pushButton_CPMatch"));
-
-        horizontalLayout_3->addWidget(pushButton_CPMatch);
-
-        pushButton_DataExport = new QPushButton(layoutWidget1);
-        pushButton_DataExport->setObjectName(QString::fromUtf8("pushButton_DataExport"));
-
-        horizontalLayout_3->addWidget(pushButton_DataExport);
-
-
-        verticalLayout_2->addLayout(horizontalLayout_3);
-
-        progressBar_MatchingProgress = new QProgressBar(layoutWidget1);
-        progressBar_MatchingProgress->setObjectName(QString::fromUtf8("progressBar_MatchingProgress"));
-        progressBar_MatchingProgress->setMaximum(0);
-        progressBar_MatchingProgress->setValue(0);
-
-        verticalLayout_2->addWidget(progressBar_MatchingProgress);
-
-        gridLayoutWidget = new QWidget(centralwidget);
-        gridLayoutWidget->setObjectName(QString::fromUtf8("gridLayoutWidget"));
-        gridLayoutWidget->setGeometry(QRect(10, 10, 741, 711));
-        gridLayout = new QGridLayout(gridLayoutWidget);
+        horizontalLayout_5 = new QHBoxLayout(centralwidget);
+        horizontalLayout_5->setObjectName(QString::fromUtf8("horizontalLayout_5"));
+        horizontalLayout_4 = new QHBoxLayout();
+        horizontalLayout_4->setObjectName(QString::fromUtf8("horizontalLayout_4"));
+        gridLayout = new QGridLayout();
         gridLayout->setObjectName(QString::fromUtf8("gridLayout"));
-        gridLayout->setContentsMargins(0, 0, 0, 0);
-        graphicsView_DispWindow = new QGraphicsView(gridLayoutWidget);
+        graphicsView_DispWindow = new QGraphicsView(centralwidget);
         graphicsView_DispWindow->setObjectName(QString::fromUtf8("graphicsView_DispWindow"));
         QPalette palette;
         QBrush brush(QColor(0, 0, 0, 255));
@@ -163,10 +90,111 @@ public:
 
         gridLayout->addWidget(graphicsView_DispWindow, 0, 0, 1, 1);
 
+
+        horizontalLayout_4->addLayout(gridLayout);
+
+        verticalLayout_3 = new QVBoxLayout();
+        verticalLayout_3->setObjectName(QString::fromUtf8("verticalLayout_3"));
+        groupBox = new QGroupBox(centralwidget);
+        groupBox->setObjectName(QString::fromUtf8("groupBox"));
+        gridLayout_4 = new QGridLayout(groupBox);
+        gridLayout_4->setObjectName(QString::fromUtf8("gridLayout_4"));
+        verticalLayout = new QVBoxLayout();
+        verticalLayout->setObjectName(QString::fromUtf8("verticalLayout"));
+        horizontalLayout = new QHBoxLayout();
+        horizontalLayout->setObjectName(QString::fromUtf8("horizontalLayout"));
+        lineEdit_PointCloudFilePath = new QLineEdit(groupBox);
+        lineEdit_PointCloudFilePath->setObjectName(QString::fromUtf8("lineEdit_PointCloudFilePath"));
+
+        horizontalLayout->addWidget(lineEdit_PointCloudFilePath);
+
+        pushButton_SelectPointCloud = new QPushButton(groupBox);
+        pushButton_SelectPointCloud->setObjectName(QString::fromUtf8("pushButton_SelectPointCloud"));
+
+        horizontalLayout->addWidget(pushButton_SelectPointCloud);
+
+
+        verticalLayout->addLayout(horizontalLayout);
+
+        horizontalLayout_2 = new QHBoxLayout();
+        horizontalLayout_2->setObjectName(QString::fromUtf8("horizontalLayout_2"));
+        lineEdit_ModelPath = new QLineEdit(groupBox);
+        lineEdit_ModelPath->setObjectName(QString::fromUtf8("lineEdit_ModelPath"));
+
+        horizontalLayout_2->addWidget(lineEdit_ModelPath);
+
+        pushButton_SelectModel = new QPushButton(groupBox);
+        pushButton_SelectModel->setObjectName(QString::fromUtf8("pushButton_SelectModel"));
+
+        horizontalLayout_2->addWidget(pushButton_SelectModel);
+
+
+        verticalLayout->addLayout(horizontalLayout_2);
+
+
+        gridLayout_4->addLayout(verticalLayout, 0, 0, 1, 1);
+
+
+        verticalLayout_3->addWidget(groupBox);
+
+        groupBox_2 = new QGroupBox(centralwidget);
+        groupBox_2->setObjectName(QString::fromUtf8("groupBox_2"));
+        gridLayout_3 = new QGridLayout(groupBox_2);
+        gridLayout_3->setObjectName(QString::fromUtf8("gridLayout_3"));
+        tableView_DataList = new QTableView(groupBox_2);
+        tableView_DataList->setObjectName(QString::fromUtf8("tableView_DataList"));
+
+        gridLayout_3->addWidget(tableView_DataList, 0, 0, 1, 1);
+
+
+        verticalLayout_3->addWidget(groupBox_2);
+
+        groupBox_3 = new QGroupBox(centralwidget);
+        groupBox_3->setObjectName(QString::fromUtf8("groupBox_3"));
+        gridLayout_5 = new QGridLayout(groupBox_3);
+        gridLayout_5->setObjectName(QString::fromUtf8("gridLayout_5"));
+        verticalLayout_2 = new QVBoxLayout();
+        verticalLayout_2->setObjectName(QString::fromUtf8("verticalLayout_2"));
+        horizontalLayout_3 = new QHBoxLayout();
+        horizontalLayout_3->setObjectName(QString::fromUtf8("horizontalLayout_3"));
+        pushButton_CPMatch = new QPushButton(groupBox_3);
+        pushButton_CPMatch->setObjectName(QString::fromUtf8("pushButton_CPMatch"));
+
+        horizontalLayout_3->addWidget(pushButton_CPMatch);
+
+        pushButton_DataExport = new QPushButton(groupBox_3);
+        pushButton_DataExport->setObjectName(QString::fromUtf8("pushButton_DataExport"));
+
+        horizontalLayout_3->addWidget(pushButton_DataExport);
+
+
+        verticalLayout_2->addLayout(horizontalLayout_3);
+
+        progressBar_MatchingProgress = new QProgressBar(groupBox_3);
+        progressBar_MatchingProgress->setObjectName(QString::fromUtf8("progressBar_MatchingProgress"));
+        progressBar_MatchingProgress->setMaximum(0);
+        progressBar_MatchingProgress->setValue(0);
+
+        verticalLayout_2->addWidget(progressBar_MatchingProgress);
+
+
+        gridLayout_5->addLayout(verticalLayout_2, 0, 0, 1, 1);
+
+
+        verticalLayout_3->addWidget(groupBox_3);
+
+
+        horizontalLayout_4->addLayout(verticalLayout_3);
+
+        horizontalLayout_4->setStretch(0, 2);
+        horizontalLayout_4->setStretch(1, 1);
+
+        horizontalLayout_5->addLayout(horizontalLayout_4);
+
         MainWindow->setCentralWidget(centralwidget);
         menubar = new QMenuBar(MainWindow);
         menubar->setObjectName(QString::fromUtf8("menubar"));
-        menubar->setGeometry(QRect(0, 0, 1186, 26));
+        menubar->setGeometry(QRect(0, 0, 1268, 23));
         MainWindow->setMenuBar(menubar);
         statusbar = new QStatusBar(MainWindow);
         statusbar->setObjectName(QString::fromUtf8("statusbar"));
